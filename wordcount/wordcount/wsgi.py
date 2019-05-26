655496690f6e64eabb08c4667317211c49c7e86e
book3620 <book3620@naver.com>
2019-05-26 20:08:06 +0900

    ss

--- wordcount/wordcount/wsgi.py
+++ wordcount/wordcount/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for workcount project.
+WSGI config for wordcount project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workcount.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordcount.settings')
 
 application = get_wsgi_application()
